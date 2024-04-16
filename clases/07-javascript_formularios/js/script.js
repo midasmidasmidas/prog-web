8525b06ddd9bab756240fe4fb01068a7db052c75
unknown <SSDD@PNAWSA06LC1011.duoc.acad>
2024-04-16 12:11:24 -0400

    js variables y boton | interpolacion de strings

--- clases/07-javascript_formularios/js/script.js
+++ clases/07-javascript_formularios/js/script.js
@@ -1,1 +1,21 @@
 console.log("chao mundo");
+
+let nombre = document.getElementById("nombre");
+let apellido = document.getElementById("apellido");
+
+let btnEnviar = document.getElementById("btnEnviar");
+
+let nombreV = "";
+let apellidoV = "";
+
+btnEnviar.addEventListener("click", (e)=> {
+            e.preventDefault();
+
+            nombreV = nombre.value
+            apellidoV = apellido.value
+
+            console.log("Nombre: " + nombreV + " Apellido: " + apellidoV); // concatenacion (usando el signo +)
+            console.log(`Nombre: ${nombreV}, Apellido: ${apellidoV}`); // interpolacion (usando el backtick, dolar, y corchetes)
+            
+            console.log("tamo bien");
+        });
