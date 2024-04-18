f90316bbcb1b304873be08b41da123a6e0da3889
unknown <SSDD@PNAWSA06LC1011.duoc.acad>
2024-04-18 12:34:20 -0400

    dividir y multiplicar

--- clases/08-javascript-formularios/js/operaciones.js
+++ clases/08-javascript-formularios/js/operaciones.js
@@ -14,3 +14,23 @@ export const restar = (n1, n2) => {
     n2 = parseInt(n2);
     return n1 - n2;
 }
+
+export const mult = (n1, n2) => {
+    
+    n1 = parseInt(n1);
+    n2 = parseInt(n2);
+
+    if(n1 == 9) {n1 = 11}
+    return n1 * n2;
+}
+
+export const dividir = (n1, n2) => {
+    
+    n1 = parseInt(n1);
+    n2 = parseInt(n2);
+
+    if(n1 == 0 || n2 == 0)
+        return "no dividir entre cero aweonao";
+    else
+        return n1 / n2;
+}
--- clases/08-javascript-formularios/js/script.js
+++ clases/08-javascript-formularios/js/script.js
@@ -1,4 +1,5 @@
-import { sumar, restar } from "./operaciones.js"; // esto solo funciona si el <script> tiene el type="module"
+// esto solo funciona si el <script> del index tiene el type="module"
+import { sumar, restar, dividir, mult } from "./operaciones.js";
 
 let numero1 = document.getElementById("numero1");
 let numero2 = document.getElementById("numero2");
@@ -7,12 +8,12 @@ let resultado = document.getElementById("resultado");
 let error1 = document.getElementById("error1");
 let error2 = document.getElementById("error2");
 
-let v_n1 = 0;
-let v_n2 = 0;
 let v_res = 0;
 
 let btnSumar = document.getElementById("btnSumar");
 let btnRestar = document.getElementById("btnRestar");
+let btnMult = document.getElementById("btnMult");
+let btnDividir = document.getElementById("btnDividir");
 
 btnSumar.addEventListener("click", function(){
 
@@ -49,6 +50,46 @@ btnRestar.addEventListener("click", function(){
     }
 
     v_res = restar(numero1.value, numero2.value);
+    resultado.innerHTML = v_res;
+    
+    console.log(`${v_res}`);
+});
+
+btnMult.addEventListener("click", function(){
+
+    if(numero1.value === "")
+    {
+        error1.innerHTML = "ERROR: Ingresar primer numero";
+        error1.style.color = "red";
+    }
+
+    if(numero2.value === "")
+    {
+        error2.innerHTML = "ERROR: Ingresar segundo numero";
+        error2.style.color = "red";
+    }
+
+    v_res = mult(numero1.value, numero2.value);
+    resultado.innerHTML = v_res;
+    
+    console.log(`${v_res}`);
+});
+
+btnDividir.addEventListener("click", function(){
+
+    if(numero1.value === "")
+    {
+        error1.innerHTML = "ERROR: Ingresar primer numero";
+        error1.style.color = "red";
+    }
+
+    if(numero2.value === "")
+    {
+        error2.innerHTML = "ERROR: Ingresar segundo numero";
+        error2.style.color = "red";
+    }
+
+    v_res = dividir(numero1.value, numero2.value);
     resultado.innerHTML = v_res;
     
     console.log(`${v_res}`);
