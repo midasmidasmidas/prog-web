ae2c83cc0f81e8fc8022ca22020ec7acf48906ae
unknown <SSDD@PNAWSA06LC1011.duoc.acad>
2024-04-18 11:25:56 -0400

    cosas clase 8

---
+++ clases/08-javascript-formularios/js/script.js
@@ -0,0 +1,29 @@
+console.log("chao mundo");
+
+let nombre = document.getElementById("nombre");
+let apellido = document.getElementById("apellido");
+let error1 = document.getElementById("error1"); // label vacio para usar como error al lado del nombre
+
+let btnEnviar = document.getElementById("btnEnviar");
+
+let nombreV = "";
+let apellidoV = "";
+
+btnEnviar.addEventListener("click", (e)=> {
+            e.preventDefault(); // previene que el formulario haga refresh a la pagina
+
+            nombreV = nombre.value
+            apellidoV = apellido.value
+
+            if(nombreV === "")
+            {
+                error1.innerHTML = "Debe ingresar nombre";
+                error1.style.color = "red";
+                return;
+            }
+
+            console.log("Nombre: " + nombreV + " Apellido: " + apellidoV); // concatenacion (usando el signo +)
+            console.log(`Nombre: ${nombreV}, Apellido: ${apellidoV}`); // interpolacion (usando el backtick, dolar, y corchetes)
+            
+            console.log("tamo bien");
+        });
