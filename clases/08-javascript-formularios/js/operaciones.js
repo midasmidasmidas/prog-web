d9e013e3ca61571ef6298f388a9d72a895c3bfdb
unknown <SSDD@PNAWSA06LC1011.duoc.acad>
2024-04-18 12:03:08 -0400

    calculadora de sumas funcional

---
+++ clases/08-javascript-formularios/js/operaciones.js
@@ -0,0 +1,14 @@
+// el "export" solo funciona si el <script> tiene el type="module"
+export const sumar = (n1, n2) => {
+    
+    n1 = parseInt(n1);
+    n2 = parseInt(n2);
+    return n1 + n2;
+}
+
+export const restar = (n1, n2) => {
+    
+    n1 = parseInt(n1);
+    n2 = parseInt(n2);
+    return n1 - n2;
+}
--- clases/08-javascript-formularios/js/script.js
+++ clases/08-javascript-formularios/js/script.js
@@ -1,11 +1,15 @@
+import { sumar, restar } from "./operaciones.js"; // esto solo funciona si el <script> tiene el type="module"
+
 let numero1 = document.getElementById("numero1");
 let numero2 = document.getElementById("numero2");
+let resultado = document.getElementById("resultado");
 
 let error1 = document.getElementById("error1");
 let error2 = document.getElementById("error2");
 
 let v_n1 = 0;
 let v_n2 = 0;
+let v_res = 0;
 
 let btnSumar = document.getElementById("btnSumar");
 
@@ -22,6 +26,9 @@ btnSumar.addEventListener("click", function(){
         error2.innerHTML = "ERROR: Ingresar segundo numero";
         error2.style.color = "red";
     }
+
+    v_res = sumar(numero1.value, numero2.value);
+    resultado.innerHTML = v_res;
     
-    console.log(`${v_n1 + v_n2}`);
+    console.log(`${v_res}`);
 });
