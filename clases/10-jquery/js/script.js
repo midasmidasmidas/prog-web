97438a0988a06be4f0070b6fedb98217a3847fe2
unknown <SSDD@PNAWSA06LC1012.duoc.acad>
2024-04-25 11:48:56 -0400

    codigo jquery

--- clases/10-jquery/js/script.js
+++ clases/10-jquery/js/script.js
@@ -1,25 +1,41 @@
-let numero1 = 0;
-let numero2 = 0;
+$("document").ready(function(){
+    let numero1 = 0;
+    let numero2 = 0;
+    let resultado = 0;
 
-$("#btnSumar").click(function()
-{
-    $("#error1").text("");
-    $("#error2").text("");
-    $("#resultado").text("");
-
-    numero1 = $("#numero1").val();
-    if(numero1 === "")
+    $("#btnSumar").click(function()
     {
-        $("#error1").text("DEBE INSERTAR PRIMER NUMERO");
-        $("#error1").css("color", "red");
-        return;
-    }
+        $("#error1").text("");
+        $("#error2").text("");
+        $("#resultado").text("");
 
-    numero2 = $("#numero2").val();
-    if(numero2 === "")
+        numero1 = $("#numero1").val();
+        if(numero1 === "")
+        {
+            $("#error1").text("DEBE INSERTAR PRIMER NUMERO");
+            $("#error1").css("color", "red");
+            return;
+        }
+
+        numero2 = $("#numero2").val();
+        if(numero2 === "")
+        {
+            $("#error2").text("DEBE INSERTAR SEGUNDO NUMERO");
+            $("#error2").css("color", "red");
+            return;
+        }
+
+        resultado = parseInt(numero1) + parseInt(numero2);
+        console.log(resultado);
+        $("#resultado").text(resultado);
+        $("#resultado").css("fontSize", "5rem");
+        limpiar();
+    });
+
+    function limpiar()
     {
-        $("#error2").text("DEBE INSERTAR SEGUNDO NUMERO");
-        $("#error2").css("color", "red");
-        return;
+        $("#numero1").val("");
+        $("#numero2").val("");
+        $("#resultado").val("");
     }
 });
