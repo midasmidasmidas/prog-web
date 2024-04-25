4c6b60d376d701f0fb28dfea962f5e4c108b256c
unknown <SSDD@PNAWSA06LC1012.duoc.acad>
2024-04-25 12:30:46 -0400

    lista de digimones, GET jquery

---
+++ clases/10-jquery/js/script-digimon.js
@@ -0,0 +1,29 @@
+// jquery se añade con un <script> en el head esta vez.
+// esta version de jquery es la min, porque con la slim no funciona la funcion $.get
+$("document").ready(function(){
+
+    $.get("https://digimon-api.vercel.app/api/digimon", function(data, status)
+    {
+        if(status === "success")
+        {
+            $.each(data, function(index, digimon){
+                let digimonCard = `
+                <div class="col-md-2 mb-3">
+                    <div class="card">
+                        <img src="${digimon.img}" class="card-img-top" alt="${digimon.name}"
+                        <div class="card-body">
+                            <h5 class="card-title">${digimon.name}</h5>
+                            <p class="card-text">Tipo:${digimon.level}</p>
+                        </div>
+                    </div>
+                </div>`
+
+                $("#digimon-list").append(digimonCard);
+            });
+        } else 
+        {
+            $("digimon-list").html("error digimon");
+        }
+    });
+
+});
