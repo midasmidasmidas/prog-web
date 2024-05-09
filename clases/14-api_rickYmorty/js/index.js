abf149b0279fc9d5fd7154786c1f8d750fb11a59
unknown <SSDD@PNAWSA06LC1011.duoc.acad>
2024-05-09 11:11:59 -0400

    base clase 14

---
+++ clases/14-api_rickYmorty/js/index.js
@@ -0,0 +1,18 @@
+const getPersonajes = async() => {
+	try {
+		const response = await fetch("https://rickandmortyapi.com/api/character");
+		const data = await response.json();
+
+		return data.results;
+	} catch(error) {
+		console.log(`El error es: ${error}`);
+	}
+}
+
+getPersonajes()
+	.then( (data) => {
+		console.log(data);
+	})
+	.catch( (error) => {
+		console.log(`El error es: ${error}`);
+	})
